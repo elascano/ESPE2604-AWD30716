40986146c04982c639c6705edb8faa7538ec64d1
Esteban Quiroga <eaquiroga@espe.edu.ec>
2026-04-20 21:05:09 -0500

    hw03 done

---
+++ hw/quiroga/u1/hw03Forms/src/js/loginAuthentication.js
@@ -0,0 +1,52 @@
+
+var loginStatus = false;
+var user = {name: "", email: "", password: ""};
+
+const login = document.getElementById("login");
+const API_URL = "https://pjdbftkrbrguiulkhnpk.supabase.co/rest/v1/users";
+const API_KEY = "sb_publishable_3IMsmLFBE-QL0WCjjtg4bA_Arkp3RjZ";
+
+login.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    user.name = document.getElementById("name").value;
+    user.email = document.getElementById("email").value;
+    user.password = document.getElementById("password").value;
+
+    loginStatus = getUser(user);
+
+    if(loginStatus){
+        window.location.href = "./src/pages/mainMenu.html";
+    } else {
+        alert("Invalid email or password. Please try again.");
+    }
+
+});
+
+async function getUser(user){
+    try {
+       const response = await fetch(`${API_URL}?email=eq.${user.email}&password=eq.${user.password}`, {
+            method: "GET",
+            headers: {
+                "apikey": API_KEY,
+                "Authorization": `Bearer ${API_KEY}`,
+                "Content-Type": "application/json"
+        }});
+
+        if(!response.ok){
+            throw new Error("Error fetching user data");
+        }
+
+        const data = await response.json();
+        
+        if(data.length > 0){
+            return data[0];
+        } else {
+            return null;
+        }    
+
+    } catch (error) {
+        console.error("Error:", error);
+        return null;
+    }        
+}
