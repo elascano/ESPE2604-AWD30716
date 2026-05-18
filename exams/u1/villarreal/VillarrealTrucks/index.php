bb1e89355745c0355a8365bd39361da947d94c33
Evelyn Haydée Villarreal Salgado <ehvillarreal@espe.edu.ec>
2026-05-18 13:06:33 -0500

    Update index.php

--- exams/u1/villarreal/VillarrealTrucks/index.php
+++ exams/u1/villarreal/VillarrealTrucks/index.php
@@ -1,7 +1,9 @@
 <?php
 
-if (file_exists(__DIR__ . '/../.env')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+require_once __DIR__ . '/vendor/autoload.php';
+
+if (file_exists(__DIR__ . '/.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 }
 
