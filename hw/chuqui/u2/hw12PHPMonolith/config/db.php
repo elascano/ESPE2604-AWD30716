09cfec0fb7076fe79a2db7d7dc693f613c342a75
Andreina Chuqui <kachuqui@espe.edu.ec>
2026-05-20 23:42:38 -0500

    hw12PHPMonolith: add .gitignore and load MONGODB_URI from .env

--- hw/chuqui/u2/hw12PHPMonolith/config/db.php
+++ hw/chuqui/u2/hw12PHPMonolith/config/db.php
@@ -1,7 +1,19 @@
 <?php
 require 'vendor/autoload.php';
 
-$mongoUri = getenv('MONGODB_URI') ?: "mongodb+srv://kachuqui_db_user:Simon123@cluster0.x7strgx.mongodb.net/?appName=Cluster0";
+$envFile = __DIR__ . '/../.env';
+if (file_exists($envFile)) {
+    foreach (file($envFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) as $line) {
+        if (str_starts_with(trim($line), '#')) continue;
+        [$key, $value] = explode('=', $line, 2);
+        putenv(trim($key) . '=' . trim($value));
+    }
+}
+
+$mongoUri = getenv('MONGODB_URI');
+if (!$mongoUri) {
+    die("Error: MONGODB_URI environment variable is not set.");
+}
 
 try {
     $client = new MongoDB\Client($mongoUri);
