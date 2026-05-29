278d32e8c6ea3eee3ae4d862cc5b13ed928527c2
Cámeron Victoria Díaz Caizaluisa <132411007+victoriadiazcaizaluisa@users.noreply.github.com>
2026-05-28 21:47:18 -0500

    Update redirect location for unauthorized access

--- hw/diaz/u2/hw14OAuth/auth/auth-check.php
+++ hw/diaz/u2/hw14OAuth/auth/auth-check.php
@@ -2,6 +2,5 @@
 require_once __DIR__ . '/session.php';
 
 if (!isset($_SESSION['user'])) {
-    header('Location: /hw13Sesion/index.php?unauthorized=true');
+    header('Location: /index.php?unauthorized=true');
     exit();
-}
