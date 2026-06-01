a91f50d5beb3f6cbebb31841c8ebd92fbe93e550
Andino Martínez Nelson David <ndandino@espe.edu.ec>
2026-06-01 13:15:28 -0500

    ws12

---
+++ ws/quiroga/u2/ws12Node/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = "127.0.0.1";
+const port = 5013;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('Hello World from <b>Node.js</b>\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Esteban's server running at http://${hostname}:${port}/`);
+});
