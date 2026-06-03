5b64aa58b86698047a4748a291b1ad571e3d40d6
EvelynVillarreal <ehvillarreal@espe.edu.ec>
2026-06-03 11:11:50 -0500

    ws13

--- ws/alvarado/u2/ws10CloudASP-JSP/obj/Debug/net8.0/SchoolGrades.AssemblyInfo.cs
+++ ws/alvarado/u2/ws10CloudASP-JSP/obj/Debug/net8.0/SchoolGrades.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolGrades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2797ede263dc344a81327bf188773f26743b933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6875a76cd76b703a82a73dd909b48a017d2491ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolGrades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolGrades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
