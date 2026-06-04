1a7a63a888bd464068ed3d38752adde1195e7ba5
victoriadiazcaizaluisa <cvdiaz3@espe.edu.ec>
2026-06-03 21:06:21 -0500

    ws14

---
+++ ws/diaz/u2/ws14RESTws/index.js
@@ -0,0 +1,16 @@
+const port = 3006
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://oop:oop@cluster0.9knxc.mongodb.net/oop?appName=Cluster0');
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Diaz System connected to mongoDB"));
+
+app.use(express.json());
+const customerRoutes = require("./routes/customerRoutes");
+const router = require('./routes/customerRoutes');
+app.use("/computerstore", customerRoutes);
+
+app.listen(port, () => console.log("Diaz's Computers Store server is running on port-->" + port));
