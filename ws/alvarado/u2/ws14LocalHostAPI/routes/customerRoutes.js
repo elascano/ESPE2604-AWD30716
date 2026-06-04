aa70cd4123948af62d000ae340e071c93970310c
Dylainous <dylanjp10@outlook.com>
2026-06-03 21:26:39 -0500

    ws14

---
+++ ws/alvarado/u2/ws14LocalHostAPI/routes/customerRoutes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const customer = require("../models/customer");
+const router = express.Router();
+
+//Get all Customers
+router.get("/customers", async (req, res) => {
+    try{
+        const customers = await customer.find();
+        res.json(customers);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get("/customers/:id", async (req, res) => {
+    try{
+        const customerObject = await customer.findOne({ id: req.params.id });
+        if (customerObject == null) {
+            res.status(400).json({status: 404});
+        } else {
+            res.json(customerObject);
+        }
+    } catch (error) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
