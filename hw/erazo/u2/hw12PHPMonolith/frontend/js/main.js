51ac77906c641b06d87164b91bf1c34ad2aa8463
unknown <lierazo@espe.edu.ec>
2026-06-12 18:56:19 -0500

    add correct files

--- hw/erazo/u2/hw12PHPMonolith/frontend/js/main.js
+++
@@ -1,7 +0,0 @@
-import { ProductView } from './views/ProductView.js';
-import { ProductController } from './controllers/ProductController.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const view = new ProductView();
-    new ProductController(view);
-});
