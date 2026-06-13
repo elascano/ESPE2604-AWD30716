7914a8d478822cef1f5261b0964049d811285d3e
EvelynVillarreal <ehvillarreal@espe.edu.ec>
2026-06-12 20:26:55 -0500

    ws18

--- ws/erazo/u2/ws10ASP_JSPCloud/erazoStudentNames/obj/Debug/net8.0/ErazoStudentNames.AssemblyInfo.cs
+++ ws/erazo/u2/ws10ASP_JSPCloud/erazoStudentNames/obj/Debug/net8.0/ErazoStudentNames.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ErazoStudentNames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+725a791a69fe4c76bba70a407a002248d0a277f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8edec9a7199d65a6ab33d72d7f819e91eddf69a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ErazoStudentNames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ErazoStudentNames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
