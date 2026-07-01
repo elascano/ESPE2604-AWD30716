ee9cf170c88a8fee19a5ba6ace919c242f58111d
GabMGallegos <gabriel25molina@hotmail.com>
2026-07-01 13:38:56 -0500

    ws27

---
+++ ws/molina/u3/ws27FrontEndLayoutCoded/index.php
@@ -0,0 +1,50 @@
+<?php
+$url = "http://universities.hipolabs.com/search?country=Ecuador";
+$response = file_get_contents($url);
+$universities = json_decode($response, true);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Workshop 27 AWD - Universities in Ecuador - Molina</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="header">
+    <h1>Ecuador Universities</h1>
+    <input type="text" id="search" placeholder="Search by Domain, or Web Domain...">
+</div>
+
+<table id="universitiesTable">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Domain</th>
+            <th>Web</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($universities as $uni): ?>
+            <tr>
+                <td><?= htmlspecialchars($uni['name']) ?></td>
+                <td><?= htmlspecialchars($uni['domains'][0]) ?></td>
+                <td>
+                    <a href="<?= htmlspecialchars($uni['web_pages'][0]) ?>" target="_blank">
+                        <?= htmlspecialchars($uni['web_pages'][0]) ?>
+                    </a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+
+<div class="no-results" id="noResults">
+    No records are found
+</div>
+
+<script src="functions.js"></script>
+</body>
+</html>
